a658482440c6f7e7718cd373cab927221f29b22e
Ha.Minh <minhhh@minhhuyha.info>
2017-03-09 15:58:52 +0700

    Add demo scene

---
+++ Assets/MeshMorphing/Test/Demo.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Demo : MonoBehaviour
+{
+    public MeshMorphing meshMorphing;
+    int currentIndex = 0;
+    int oldIndex = -1;
+    float curWeight = 0;
+
+    public void Update ()
+    {
+        curWeight += 0.01f;
+        if (curWeight > 1) {
+            meshMorphing.ChangeWeight (currentIndex, 1);
+            curWeight = 0;
+            oldIndex = currentIndex;
+            currentIndex += 1;
+            if (currentIndex > 3) {
+                currentIndex = 0;
+            }
+        } else {
+            if (oldIndex != -1) {
+                var w = meshMorphing.GetWeight (oldIndex) - 0.01f;
+                if (w < 0) {
+                    w = 0;
+                }
+
+                meshMorphing.ChangeWeight (oldIndex, w);
+            }
+            meshMorphing.ChangeWeight (currentIndex, curWeight);
+        }
+    }
+
+}
